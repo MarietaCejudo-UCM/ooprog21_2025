75948ee1144caed0770baa5369758d5233c19669
MarietaCejudo-UCM <cejudomarieta@gmail.com>
2025-09-03 20:58:12 +0800

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,18 @@
+public class Comments {
+    public static void main (String[] args){
+
+         /**
+         * Hello!
+         * This is an example of a documentation comment.
+        */
+
+        // This is a single-line comment.
+
+        /* This is a multi-line comment.
+          It can span several lines
+          and are used for longer explanations or to comment out blocks of code.
+        */
+    }
+
+    
+}
---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,12 @@
+
+public class FavoriteSong {
+    public static void main(String[] args){
+
+        System.out.println("I'm doin' me regardless");
+        System.out.println("And I don't care what you say about it");
+        System.out.println("And it don't matter if you like it or not");
+        System.out.println("I'm having all this fun, so why would I ever stop?");
+    }
+
+
+}
